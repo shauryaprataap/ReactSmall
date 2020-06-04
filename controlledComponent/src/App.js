a8f7f50022b14ff8f0050dbc126a4c01addbaeea
shauryaprataap <theshauryapratap@gmail.com>
2020-06-04 23:37:11 +0530

    Controlled Component

---
+++ controlledComponent/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    query: "",
+  };
+
+  handleChange = (event) => {
+    this.setState({ query: event.target.value });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">ReactND - Coding Practice</h1>
+        </header>
+        <div className="container">
+          <input
+            type="text"
+            placeholder="Say Something"
+            value={this.state.query}
+            onChange={this.handleChange}
+          />
+          <p className="echo">Echo:</p>
+          <p>{this.state.query}</p>
+          <p>This should mirror the text you typed into the input field.</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
